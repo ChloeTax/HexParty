075aeab611c3789e060872420c9d5eb97bb1c460
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-12-07 11:30:41 -0600

    Re-implemented Sockets via Socket Impetus
    Swapped to using proper mavens for certain Gradle dependencies
    
    Added Socket-based patterns
    
    Made a blacklist tag for deserialization

--- common/src/main/kotlin/io/github/techtastic/hexweb/HexWeb.kt
+++ common/src/main/kotlin/io/github/techtastic/hexweb/HexWeb.kt
@@ -1,12 +1,19 @@
 package io.github.techtastic.hexweb
 
+import com.mojang.logging.LogUtils
+import io.github.techtastic.hexweb.blocks.HexWebBlockEntities
+import io.github.techtastic.hexweb.blocks.HexWebBlocks
 import io.github.techtastic.hexweb.config.HexWebConfig
 
 object HexWeb {
     const val MOD_ID: String = "hexweb"
+    val LOGGER = LogUtils.getLogger()
 
     @JvmStatic
     fun init() {
+        HexWebBlocks.register()
+        HexWebBlockEntities.register()
+
         HexWebConfig.setup()
     }
 }
---
+++ common/src/main/kotlin/io/github/techtastic/hexweb/blocks/circles/impetuses/BlockSocketImpetus.kt
@@ -0,0 +1,32 @@
+package io.github.techtastic.hexweb.blocks.circles.impetuses
+
+import at.petrak.hexcasting.api.block.circle.BlockAbstractImpetus
+import io.github.techtastic.hexweb.blocks.HexWebBlockEntities
+import net.minecraft.core.BlockPos
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityTicker
+import net.minecraft.world.level.block.entity.BlockEntityType
+import net.minecraft.world.level.block.state.BlockState
+
+
+class BlockSocketImpetus(properties: Properties) : BlockAbstractImpetus(properties) {
+    override fun newBlockEntity(blockPos: BlockPos, blockState: BlockState) = BlockEntitySocketImpetus(blockPos, blockState)
+
+    override fun <T : BlockEntity?> getTicker(
+        pLevel: Level,
+        pState: BlockState,
+        blockEntityType: BlockEntityType<T>
+    ): BlockEntityTicker<T>? {
+        return if (!pLevel.isClientSide)
+            createTickerHelper(blockEntityType, HexWebBlockEntities.SOCKET_IMPETUS.get(), BlockEntitySocketImpetus::serverTick)
+        else
+            null
+    }
+
+    protected fun <E : BlockEntity?, A : BlockEntity?> createTickerHelper(
+        type: BlockEntityType<A>, targetType: BlockEntityType<E>, ticker: BlockEntityTicker<in E>?
+    ): BlockEntityTicker<A>? {
+        return if (targetType === type) ticker as? BlockEntityTicker<A> else null
+    }
+}
