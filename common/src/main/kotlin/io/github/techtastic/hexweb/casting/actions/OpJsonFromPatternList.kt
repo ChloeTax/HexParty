40e9323005ad9fedbefec6df0db219e7f2b46600
Caelum <cael>
2026-02-14 20:16:35 +0300

    Added list -> json

---
+++ common/src/main/kotlin/io/github/techtastic/hexweb/casting/actions/OpJsonFromPatternList.kt
@@ -0,0 +1,21 @@
+package io.github.techtastic.hexweb.casting.actions
+
+import at.petrak.hexcasting.api.casting.castables.ConstMediaAction
+import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
+import at.petrak.hexcasting.api.casting.getList
+import at.petrak.hexcasting.api.casting.iota.DoubleIota
+import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.casting.iota.ListIota
+import io.github.techtastic.hexweb.HexWeb
+import io.github.techtastic.hexweb.casting.iota.JsonIota
+import io.github.techtastic.hexweb.utils.IotaParser
+
+object OpJsonFromPatternList : ConstMediaAction {
+    override val argc: Int
+        get() = 1
+
+    override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> {
+        val list = args.getList(0,argc)
+        return listOf(JsonIota(IotaParser.json_from_list(list)))
+    }
+}
